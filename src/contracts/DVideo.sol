064fa3ca94fbd4eb9ecf502ab47c142c04419d0e
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-02-04 23:06:45 +0900

    setting up

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,32 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract DVideo {
-  uint public videoCount = 0;
-  string public name = "DVideo";
-  //Create id=>struct mapping
+    uint256 public videoCount = 0;
+    string public name = "DVideo";
 
-  //Create Struct
+    constructor() public {}
 
-
-  //Create Event
-
-
-  constructor() public {
-  }
-
-  function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
-
-    // Make sure video title exists
-
-    // Make sure uploader address exists
-
-
-    // Increment video id
-
-    // Add video to the contract
-
-    // Trigger an event
-
-  }
+    function uploadVideo(string memory _videoHash, string memory _title)
+        public
+    {}
 }
