0ab314deaed8caa1371ab2d0290e36fa69460c0c
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-02-10 17:11:22 +0900

    upload

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 contract DVideo {
     uint256 public videoCount = 0;
     string public name = "DVideo";
+    mapping(uint256 => Video) public videos;
 
     struct video {
         uint256 id;
@@ -15,5 +16,8 @@ contract DVideo {
 
     function uploadVideo(string memory _videoHash, string memory _title)
         public
-    {}
+    {
+        uint256 id = 1;
+        videos[id] = Video(id, _videoHash, title, msg.sender);
+    }
 }
