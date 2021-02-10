07337ba5f9aa7f33059979678ef5b7ed323a4eb9
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-02-10 17:05:22 +0900

    video

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -4,6 +4,13 @@ contract DVideo {
     uint256 public videoCount = 0;
     string public name = "DVideo";
 
+    struct video {
+        uint256 id;
+        string hash;
+        string title;
+        address author;
+    }
+
     constructor() public {}
 
     function uploadVideo(string memory _videoHash, string memory _title)
