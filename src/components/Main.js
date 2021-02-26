dff7a300064011772de24c0f8fe2df5ecda6ed49
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-02-27 08:35:23 +0900

    returning video

--- src/components/Main.js
+++ src/components/Main.js
@@ -53,9 +53,22 @@ class Main extends Component {
                 </div>
                   <div>
                     {/* Change Video...*/}
-                    {/* Return Side Videos...*/}
+                    return(
+                <div className="card mb-4 text-center bg-secondary mx-auto" style={{ width: '175px'}} key={key} >
+                  <div className="card-title bg-dark">
+                    <small className="text-white"><b>{video.title}</b></small>
                   </div>
-              </div>
+                  <div>
+                    <p onClick={() => this.props.changeVideo(video.hash, video.title)}>
+                      <video
+                        src={`https://ipfs.infura.io/ipfs/${video.hash}`}
+                        style={{ width: '150px' }}
+                      />
+                    </p>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       </div>
